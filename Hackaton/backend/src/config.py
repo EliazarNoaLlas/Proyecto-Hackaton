76ff9d4cf704ed74d8686d0eea160897e8b52122
Eliazar <193003@unsaac.edu.pe>
2024-09-20 07:52:17 -0500

    Proyecto Inicial

--- Hackaton/backend/src/config.py
+++
@@ -1,3 +0,0 @@
-# src/config.py
-
-API_BASE_URL = "https://firms.modaps.eosdis.nasa.gov/api/area/csv/a4af9f9db9f15fd4ceb35731121ce9fc/VIIRS_NOAA20_NRT"
--- Hackaton/backend/src/routes/routes.py
+++
@@ -1,9 +0,0 @@
-# src/routes/routes.py
-
-from flask import Flask
-from src.controllers.controller import controller_bp
-
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(controller_bp)
-    return app
